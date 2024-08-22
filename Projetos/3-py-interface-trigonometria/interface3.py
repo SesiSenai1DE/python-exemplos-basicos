0a6f21c08d18911950425ba033d7d19420620409
SesiSenai1DE <130493869+SesiSenai1DE@users.noreply.github.com>
2024-08-22 11:10:52 -0300

    Início Projeto
    Início Projeto

---
+++ Projetos/3-py-interface-trigonometria/interface3.py
@@ -0,0 +1,15 @@
+import tkinter as tk #Biblioteca para criação de interface
+import math #Biblioteca para realizar operações matemáticas
+from PIL import Image, ImageTk #Manipula imagens
+import os #Arquivos de sistema
+import sys #Variáveis de sistema
+
+# Configuração da janela principal
+janela = tk.Tk() #Cria a janela principal
+janela.title("Calculadora Trigonométrica") #Título
+janela.geometry("400x550") #Tamanho
+janela.configure(bg="#f0f0f0") #Cor de fundo
+
+
+# Iniciar Janela
+janela.mainloop() #Roda a tela e mantem aberta
